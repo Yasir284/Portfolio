dbc0a51a9f7915f053cbe903dee869910bbf2362
Yasir284 <lambawalay@gmail.com>
2023-02-18 20:18:31 +0530

    adjusted scorll offset

--- src/helpers/navItems.js
+++ src/helpers/navItems.js
@@ -3,31 +3,31 @@ const navItems = [
     id: 1,
     name: "Home",
     link: "home",
-    offset: -70,
+    offset: -68,
   },
   {
     id: 2,
     name: "About",
     link: "about",
-    offset: -60,
+    offset: -64,
   },
   {
     id: 3,
     name: "Skills",
     link: "skills",
-    offset: -60,
+    offset: -75,
   },
   {
     id: 4,
     name: "Projects",
     link: "projects",
-    offset: -58,
+    offset: -70,
   },
   {
     id: 5,
     name: "Contact",
     link: "contact",
-    offset: -65,
+    offset: -68,
   },
 ];
 
