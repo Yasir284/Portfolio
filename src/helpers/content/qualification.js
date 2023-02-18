ffa05696993058c7fd3a69da24971d23571171ac
Yasir284 <lambawalay@gmail.com>
2023-02-18 19:36:27 +0530

    change in the description

--- src/helpers/content/qualification.js
+++ src/helpers/content/qualification.js
@@ -3,19 +3,19 @@ const qualifications = [
     qualification: "Bachelor of engineering",
     year: "June 2022",
     description:
-      "I have completed my Btech in mechanical engineering at Gujarat Technological University, from S.V.I.T, Vasad.",
+      "I have completed my B-tech in mechanical engineering with 8.4 CGPA, at Gujarat Technological University, from S.V.I.T, Vasad.",
   },
   {
     qualification: "Higher Secondary Education",
     year: "March 2018",
     description:
-      "I have completed my higher secondary education from Baroda public school, Vadadora.",
+      "I have completed my higher secondary education with 78.48 percentile rank, from Baroda public school, Vadadora.",
   },
   {
     qualification: "Secondary Education",
     year: "March 2016",
     description:
-      "I have completed my secondary education from Zenith high school, Vadadora.",
+      "I have completed my secondary education with 95.03 percentile rank, from Zenith high school, Vadadora.",
   },
 ];
 
