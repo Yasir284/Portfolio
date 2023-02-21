e8100a74125a818d57ffac98caaf8e56667bd8dc
Yasir284 <lambawalay@gmail.com>
2023-02-21 20:12:00 +0530

    added active link function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,17 @@ import { MdClose } from "react-icons/md";
 // Helper
 import navItems from "../helpers/navItems";
 
+const navVarient = {
+  initial: { opacity: 0, y: "-100vh" },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: "-100vh" },
+  transition: { type: "just" },
+};
+
 export default function Navbar() {
   const [active, setActive] = useState(true);
+
+  const [activeListId, setActiveListId] = useState(null);
 
   function getCompInView(component) {
     component?.scrollIntoView({
@@ -20,47 +29,65 @@ export default function Navbar() {
     });
   }
 
+  window.addEventListener("scroll", function () {
+    let section = document.querySelectorAll("section");
+    let i = 0;
+    while (
+      i < section.length &&
+      section[i]?.getBoundingClientRect().top <= window.innerHeight / 2
+    ) {
+      setActiveListId(section[i].id);
+      i++;
+    }
+  });
+
   return (
-    <nav className="sticky top-0 right-0 z-20 backdrop-blur-md backdrop-filter">
-      <div className="flex flex-row items-center justify-between border-b border-black-700 py-3 px-6 font-Roboto tracking-widest sm:py-4 md:px-20">
-        <div
-          onClick={() => getCompInView(document.getElementById(navItems[0].id))}
-          className="cursor-pointer"
-        >
-          <h1 className="font-Balsamiq text-2xl font-bold">My Portfolio</h1>
+    <>
+      <nav className="sticky top-0 right-0 z-20 backdrop-blur-md backdrop-filter">
+        <div className="flex flex-row items-center justify-between border-b border-black-700 py-3 px-6 font-Roboto tracking-widest sm:py-4 md:px-20">
+          <div
+            onClick={() =>
+              getCompInView(document.getElementById(navItems[0].id))
+            }
+            className="cursor-pointer"
+          >
+            <h1 className="font-Balsamiq text-2xl font-bold">My Portfolio</h1>
+          </div>
+
+          <ul className="hidden flex-row items-center gap-6 uppercase md:flex">
+            {navItems.map((list) => (
+              <li
+                key={list.id}
+                onClick={() => getCompInView(document.getElementById(list.id))}
+                className={`cursor-pointer rounded-md border-2 border-transparent px-4 py-2 font-bold transition-all duration-200 ease-in-out hover:border-white active:scale-90 ${
+                  activeListId === list.id ? "bg-white text-black" : ""
+                }`}
+              >
+                {list.name}
+              </li>
+            ))}
+          </ul>
+          {/* Hamburger menu button */}
+          <button
+            onClick={() => setActive(!active)}
+            className="block text-white transition-all duration-200 ease-in-out active:scale-75 md:hidden"
+          >
+            {active ? (
+              <GiHamburgerMenu className="text-[1.5rem] sm:text-[2rem]" />
+            ) : (
+              <MdClose className="text-[1.5rem] sm:text-[2rem]" />
+            )}
+          </button>
         </div>
+      </nav>
 
-        <ul className="hidden flex-row items-center gap-6 uppercase md:flex">
-          {navItems.map((list) => (
-            <li
-              key={list.id}
-              onClick={() => getCompInView(document.getElementById(list.id))}
-              className="cursor-pointer rounded-md border-2 border-transparent px-4 py-2 transition-all duration-200 ease-in-out hover:border-white active:scale-90"
-            >
-              {list.name}
-            </li>
-          ))}
-        </ul>
-
-        <button
-          onClick={() => setActive(!active)}
-          className="block text-white transition-all duration-200 ease-in-out active:scale-75 md:hidden"
-        >
-          {active ? (
-            <GiHamburgerMenu className="text-[1.5rem] sm:text-[2rem]" />
-          ) : (
-            <MdClose className="text-[1.5rem] sm:text-[2rem]" />
-          )}
-        </button>
-      </div>
+      {/* Hamburger menu*/}
       <AnimatePresence>
         {active === false && (
           <motion.div
             key={active}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="flex flex-col items-center justify-center bg-white bg-opacity-10 backdrop-blur-sm backdrop-filter"
+            {...navVarient}
+            className="fixed top-0 right-0 z-10 flex h-screen w-full flex-col items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm backdrop-filter"
           >
             <motion.ul className="w-full flex-col items-center uppercase md:flex">
               {navItems.map((list) => (
@@ -69,7 +96,7 @@ export default function Navbar() {
                     getCompInView(document.getElementById(list.id))
                   }
                   key={list.id}
-                  className="w-full cursor-pointer border-b border-black-700 py-3 text-center transition-all duration-300 ease-in-out hover:bg-[rgba(255,255,255,0.53)]"
+                  className="w-full cursor-pointer py-6 text-center text-base font-bold transition-all duration-300 ease-in-out hover:bg-[rgba(255,255,255,0.53)]"
                 >
                   <p>{list.name}</p>
                 </li>
@@ -78,6 +105,6 @@ export default function Navbar() {
           </motion.div>
         )}
       </AnimatePresence>
-    </nav>
+    </>
   );
 }
