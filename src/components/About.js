53a71773df95e6d483b798ce38f5df23b825eec9
Yasir284 <lambawalay@gmail.com>
2023-02-27 18:45:32 +0530

    changed some animation

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ import { HiOutlineDownload } from "react-icons/hi";
 
 // Components and helpers
 import qualifications from "../helpers/content/qualification";
-import AnimatedWord from "./sub-components/AnimatedWord";
 
 // Images
 import myImage from "../assets/images/my-image.png";
@@ -27,36 +26,36 @@ const imageVaritent = {
 };
 
 const containerVarient = {
-  initial: { opacity: 0, x: -30 },
-  whileInView: { opacity: 1, x: 0, transition: { duration: 1.5 } },
+  initial: { opacity: 0, y: "50vh" },
+  whileInView: { opacity: 1, y: 0, transition: { duration: 1 } },
+  viewport: { once: true },
 };
 
 export default function About() {
   return (
     <section id="about" className="custom-bg min-h-screen py-10 px-6 md:px-20">
-      <motion.div
-        {...containerVarient}
-        viewport={{ once: true }}
-        className="flex flex-col gap-6"
-      >
+      <div className="flex flex-col gap-6">
         <h2 className="text-center text-4xl font-extrabold">
           About me
           <hr className="border-black-700" />
         </h2>
 
-        <div className="flex flex-col gap-12 md:flex-row md:gap-6">
+        <motion.div
+          {...containerVarient}
+          className="flex flex-col gap-12 md:flex-row md:gap-6"
+        >
           <div className="flex basis-[30%] flex-col-reverse items-center gap-6 sm:flex-col">
             <div className="flex flex-col items-center gap-6">
               <motion.div
                 {...imageVaritent}
                 className="h-52 w-36 overflow-hidden rounded-3xl bg-black-800 shadow-lg shadow-black-800 md:h-64 md:w-48"
               >
-                <img src={myImage} alt="Yasir" />
+                <img src={myImage} loading="lazy" alt="Yasir" />
               </motion.div>
 
               <button>
                 <a
-                  href="https://drive.google.com/file/d/1-CGYUTV3lUPzkYFIq9J7wq7tMI2dZ_fa/view?usp=share_link"
+                  href="https://drive.google.com/file/d/1Y5--CIp58m7ovL8XUR5ywbCV8wRYuLrh/view?usp=share_link"
                   target="_blank"
                   rel="noreferrer"
                   className="flex items-center justify-center gap-4 rounded-full border border-white bg-white px-4 py-2 text-black transition-all duration-200  ease-in-out hover:bg-transparent hover:text-white"
@@ -79,7 +78,7 @@ export default function About() {
                   className="relative flex flex-col before:absolute before:top-2 before:left-[-30px] before:z-[1] before:h-4 before:w-4 before:rounded-full before:bg-violet-400 sm:pb-6"
                 >
                   <h3 className="text-xl font-semibold tracking-wide">
-                    <AnimatedWord text={data.qualification} viewOnce={true} />
+                    {data.qualification}
                   </h3>
                   <time className="text-xs uppercase tracking-wide text-gray-400">
                     {data.year}
@@ -89,8 +88,8 @@ export default function About() {
               ))}
             </div>
           </div>
-        </div>
-      </motion.div>
+        </motion.div>
+      </div>
     </section>
   );
 }
