d47b01ef2ec8a4e718cd067800f101949a592839
Yasir284 <lambawalay@gmail.com>
2023-04-30 17:36:10 +0530

    added previous and next button to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Click } from "../assets/icons/icons";
 import { AnimatePresence, motion } from "framer-motion";
 
 // Components and helpers
 import projects from "../helpers/content/projects";
 import ProjectDetail from "./modal/ProjectDetail";
+
+// Icons
+import { ArrowLeft, ArrowRight } from "../assets/icons/icons";
 
 const cardVarient = {
   initial: { opacity: 0 },
@@ -68,13 +71,21 @@ export default function Projects() {
       },
     },
   ];
-
+  const sliderRef = useRef();
   const [category, setCategory] = useState("Javascript");
   const [acitveId, setActiveId] = useState(itemList[2].id);
   const [activeModal, setActiveModal] = useState({
     active: false,
     project: "",
   });
+
+  const slidePrev = () => {
+    sliderRef.current.scrollLeft -= sliderRef.current.children[0].clientWidth;
+  };
+
+  const slideNext = () => {
+    sliderRef.current.scrollLeft += sliderRef.current.children[0].clientWidth;
+  };
 
   return (
     <section id="projects" className="custom-bg min-h-screen py-10">
@@ -99,46 +110,73 @@ export default function Projects() {
           ))}
         </ul>
 
-        {/* Projects */}
-        <AnimatePresence mode="wait">
-          <div className="myScrollbar mt-4 flex flex-shrink-0 flex-row justify-center gap-8 overflow-x-auto rounded-md py-4 transition-all ease-in-out sm:py-12 md:w-[86vw]">
-            {projects
-              .filter((e) => e.category === category)
-              .reverse()
-              .map((project, index) => (
-                <motion.div
-                  {...cardVarient}
-                  key={index}
-                  layout="position"
-                  className="group flex h-fit flex-col items-center gap-6 rounded-md active:scale-90"
-                >
-                  <div className="relative flex aspect-video w-80 items-center justify-center rounded-md border border-black-700 bg-black bg-opacity-25 shadow-lg shadow-black backdrop-blur-sm backdrop-filter sm:w-96">
-                    <img
-                      src={project.images[0]}
-                      className="aspect-video w-full rounded-md object-contain"
-                      loading="lazy"
-                      alt={project.name + " image"}
-                    />
+        <div className="flex flex-row items-center gap-2">
+          {/* Previous button */}
+          <button
+            onClick={slidePrev}
+            className="group aspect-square h-fit w-fit rounded-full border px-1 opacity-70 transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-100 active:scale-75"
+          >
+            <ArrowLeft size="1.5rem" />
+          </button>
 
-                    <div
-                      onClick={() => setActiveModal({ active: true, project })}
-                      className="absolute top-0 right-0 flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-md bg-black bg-opacity-30 opacity-0 shadow-inner shadow-black-700 backdrop-blur-sm backdrop-filter transition-all duration-300 ease-in-out group-hover:opacity-100"
-                    >
-                      <p className="text-xl font-bold">Click to know more</p>
-                      <Click size="2rem" className="animate-ping" />
-                    </div>
-                  </div>
-
-                  <motion.h2
-                    {...textVarient}
-                    className="w-fit border-b border-black-700 text-center text-2xl font-bold"
+          {/* Projects */}
+          <AnimatePresence mode="wait">
+            <motion.div
+              ref={sliderRef}
+              // onWheel={(e) => {
+              //   e.preventDefault();
+              //   sliderRef.current.scrollLeft += e.deltaY;
+              // }}
+              className="myScrollbar mx-auto mt-4 flex flex-shrink-0 snap-x snap-mandatory flex-row gap-8 overflow-x-auto scroll-smooth rounded-md py-4 transition-all duration-500 ease-in-out sm:py-12 md:w-[96%]"
+            >
+              {projects
+                .filter((e) => e.category === category)
+                .reverse()
+                .map((project) => (
+                  <motion.div
+                    {...cardVarient}
+                    key={project.id}
+                    layout="position"
+                    className="group flex h-fit snap-start flex-col items-center gap-6 rounded-md active:scale-90"
                   >
-                    {project.name}
-                  </motion.h2>
-                </motion.div>
-              ))}
-          </div>
-        </AnimatePresence>
+                    <div className="relative flex aspect-video w-80 items-center justify-center rounded-md border border-black-700 bg-black bg-opacity-25 shadow-lg shadow-black backdrop-blur-sm backdrop-filter sm:w-96">
+                      <img
+                        src={project.images[0]}
+                        className="aspect-video w-full rounded-md object-contain"
+                        loading="lazy"
+                        alt={project.name + " image"}
+                      />
+
+                      <div
+                        onClick={() =>
+                          setActiveModal({ active: true, project })
+                        }
+                        className="absolute top-0 right-0 flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-md bg-black bg-opacity-30 opacity-0 shadow-inner shadow-black-700 backdrop-blur-sm backdrop-filter transition-all duration-300 ease-in-out group-hover:opacity-100"
+                      >
+                        <p className="text-xl font-bold">Click to know more</p>
+                        <Click size="2rem" className="animate-ping" />
+                      </div>
+                    </div>
+
+                    <motion.h2
+                      {...textVarient}
+                      className="w-fit border-b border-black-700 text-center text-2xl font-bold"
+                    >
+                      {project.name}
+                    </motion.h2>
+                  </motion.div>
+                ))}
+            </motion.div>
+          </AnimatePresence>
+
+          {/* Next button */}
+          <button
+            onClick={slideNext}
+            className="group aspect-square h-fit w-fit rounded-full border px-1 opacity-70  transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-100 active:scale-75"
+          >
+            <ArrowRight size="1.5rem" />
+          </button>
+        </div>
       </div>
 
       <ProjectDetail
